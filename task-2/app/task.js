07dea5a18efe25966d1b27739f5b32d8a9f5cd2e
Alexander Logozinsky <alogozinsky@gmail.com>
2017-10-31 20:11:00 +0200

    all done

--- task-2/app/task.js
+++ task-2/app/task.js
@@ -4,6 +4,29 @@ function taskController($scope) {
   $scope.items = [
     'Some Item', 'Another Item'
   ];
+  $scope.addNewItem = function(){
+    if ($scope.tempInput){
+      $scope.items.push($scope.tempInput);
+      $scope.tempInput = "";
+    } else {
+      console.log('input is empty');
+    }
+  };
+  $scope.editItem = function(item){
+    var index = $scope.items.indexOf(item);
+    $scope.showEdit = true;
+
+    $scope.saveEdit = function(item){
+      $scope.items.splice(index, 1, $scope.editInput);
+      $scope.showEdit = false;
+      $scope.editInput = "";
+    };
+  };
+  
+  $scope.deleteItem = function(item){
+    var index = $scope.items.indexOf(item);
+    $scope.items.splice(index, 1);
+  };
 }
 
 taskApp.controller('taskController', taskController);
